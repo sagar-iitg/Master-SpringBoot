aab0be230d6097597f1218b12cce8473180761f0
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-09 13:04:58 +0530

    Added SpringRest Project

---
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,94 @@
+package com.springrest.springrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.dao.CourseDao;
+import com.springrest.springrest.entities.Course;
+
+
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	
+//	List<Course> list;
+	
+	@Autowired
+	private CourseDao courseDao;
+	
+	public CourseServiceImpl() {
+	
+		
+//		list=new ArrayList<>();
+//		list.add(new Course(1,"Core java","Basics Of java"));
+//		list.add(new Course(2,"Spring Boot","frameworks of java"));
+		
+		
+		
+		
+	}
+
+
+	@Override
+	public List<Course> getCourses() {
+		// TODO Auto-generated method stub
+//		System.out.println(list);
+		return courseDao.findAll();
+	}
+
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public Course getCourse(long courseId) {
+		// TODO Auto-generated method stub
+//		Course c=null;
+//		for(Course i:list) {
+//			if(i.getId()==courseId) {
+//				c=i;
+//				break;
+//			}
+//		}
+		return courseDao.getOne(courseId);
+	}
+
+
+	@Override
+	public Course addCourse(Course course) {
+		// TODO Auto-generated method stub
+//		list.add(course);
+		courseDao.save(course);
+		return course;
+		
+	}
+
+
+	@Override
+	public Course updateCourse(Course course) {
+		// TODO Auto-generated method stub
+//		list.forEach(e->{
+//			if(e.getId()==course.getId()) {
+//				e.setTitle(course.getTitle());
+//				e.setDescription(course.getDescription());
+//			}
+//		});
+		courseDao.save(course);
+		return course;
+	}
+
+
+	@Override
+	public void deleteCourse(long parseLong) {
+		// TODO Auto-generated method stub
+//		list=this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
+		Course entity=courseDao.getOne(parseLong);
+		courseDao.delete(entity);
+		
+	}
+
+}
