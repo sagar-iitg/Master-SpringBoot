8d9e1a6b0df0471c31e058f6b19829e618580c16
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-13 09:53:55 +0530

    added journal App

---
+++ journalApp/src/main/java/com/sagar/journalApp/controller/Home.java
@@ -0,0 +1,16 @@
+package com.sagar.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/home")
+public class Home {
+
+
+    @GetMapping
+    public String home(){
+        return "Journal App";
+    }
+}
