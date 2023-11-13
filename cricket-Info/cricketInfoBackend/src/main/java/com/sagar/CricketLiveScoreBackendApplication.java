1b4496495a2975f1bd268f4074659b48ef01804a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-14 01:06:19 +0530

    Cricket backend project

---
+++ cricket-Info/cricketInfoBackend/src/main/java/com/sagar/CricketLiveScoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketLiveScoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketLiveScoreBackendApplication.class, args);
+	}
+
+}
