23a8dcd16edd73bc05653943222a6df0d7ec4ea2
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-12-23 23:48:19 +0530

    Create HomeController.java

---
+++ springsecurity/src/main/java/com/sagar/springsecurity/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package com.sagar.springsecurity.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+
+
+    @GetMapping("/normal")
+    public ResponseEntity<String> normalUser(){
+        return ResponseEntity.ok("Yes I am normal User");
+
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminUser(){
+        return ResponseEntity.ok("Yes I am admin User");
+
+    }
+
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicUser(){
+        return ResponseEntity.ok("Yes I am public User");
+
+    }
+
+
+}
