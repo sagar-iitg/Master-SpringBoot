26ff7ed394a34134a47d9f853fd244a511dcd94f
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-12-24 14:29:55 +0530

    Create User.java

---
+++ JwtExample/src/main/java/com/sagar/User.java
@@ -0,0 +1,18 @@
+package com.sagar;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    private String userId;
+    private String name;
+    private String email;
+
+
+}
