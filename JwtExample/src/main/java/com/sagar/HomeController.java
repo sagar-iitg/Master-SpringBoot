ffcf1f399e3da43b6c8715028cb7e8b1ae847cf9
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-12-24 14:29:43 +0530

    Create HomeController.java

---
+++ JwtExample/src/main/java/com/sagar/HomeController.java
@@ -0,0 +1,34 @@
+package com.sagar;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+
+    @Autowired
+    private UserService userService;
+    @GetMapping("/user")
+    public List<User> getUser(){
+        return this.userService.getUsers();
+    }
+
+
+
+    @GetMapping("/current-user")
+    public String getLoggedInUser(Principal principal){
+        return principal.getName();
+
+    }
+
+
+
+}
