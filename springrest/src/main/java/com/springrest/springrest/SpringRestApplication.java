335970df2607fcdc474d8e1f749989e98421ac3e
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-11 21:18:05 +0530

    Update SpringRestApplication.java

--- springrest/src/main/java/com/springrest/springrest/SpringRestApplication.java
+++ springrest/src/main/java/com/springrest/springrest/SpringRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringrestApplication {
+public class SpringRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrestApplication.class, args);
+		SpringApplication.run(SpringRestApplication.class, args);
 	}
 	
 	
